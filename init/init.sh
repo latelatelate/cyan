19cc6cbbdfbf81c0547af3c4aac3b206001a14c5
latelatelate <nickmow+github@gmail.com>
2025-12-21 08:23:08 -0800

    Allow rootless to open ports >= 80

--- init/init.sh
+++ init/init.sh
@@ -4,15 +4,21 @@
 # TODO: Build out proper initilization + automation script
 #
 
-# Base froject folder
+# Base project folder
 mkdir -p /opt/cyan
 
 git clone -b dev --verbose cyan:latelatelate/cyan.git /opt/cyan 
 
 # Quadlet setup
 mkdir -p ~/.config/containers/systemd/cyan
-ln -s /opt/cyan/quadlets/* ~/.config/containers/systemd/cyan/
+rsync -avu --delete /opt/cyan/quadlets/ ~/.config/containers/systemd/cyan
 
 # SSL Certs
 mkdir -p /opt/cyan/certs
-cd /opt/cyan/certs
+
+# Allow unprivileged user to open sockets >= 80
+echo "net.ipv4.ip_unprivileged_port_start=80" > /etc/sysctl.d/99-unprivileged-port.conf
+sysctl -p /etc/sysctl.d/99-unprivileged-port.conf
+
+# Enable linger for rootless user
+loginctl enable-linger nm
