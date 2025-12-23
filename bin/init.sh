d69e1235bc8bc49884fe807541183c7a3bc5520b
latelatelate <nickmow+github@gmail.com>
2025-12-22 21:00:33 -0800

    Prep for merge with main

--- bin/init.sh
+++ bin/init.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 #
-# Initial configuration for podman host
-# TODO: Build out proper initilization + automation script
+# Initial configuration for cyan pod
 #
+set -e
 PROJECT_NAME=cyan
 INSTALL_DIR=/opt/${PROJECT_NAME}
-QUADLET_DIR=~/.config/containers/systemd/${PROJECT_NAME}
-SOCKET_DIR=~/.config/systemd/user
-
-# Base project folder
-mkdir -p ${INSTALL_DIR}
-
-set -e
+QUADLET_DIR=$HOME/.config/containers/systemd/${PROJECT_NAME}
+SOCKET_DIR=$HOME/.config/systemd/user
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
-sudo mv "$REPO_DIR" /opt/
+# Create cyan dir and move repo into it
+mkdir -p ${INSTALL_DIR}
+mv "$REPO_DIR" /opt/
 
 # Quadlet setup
 mkdir -p ${QUADLET_DIR}
 mkdir -p ${SOCKET_DIR}
 rsync -avu --delete ${INSTALL_DIR}/quadlets/ ${QUADLET_DIR}
 cp -r ${INSTALL_DIR}/sockets/ ${SOCKET_DIR}
-
-systemctl --user daemon-reload
 
 # SSL Certs
 mkdir -p ${INSTALL_DIR}/certs
@@ -35,6 +30,8 @@ sysctl -p /etc/sysctl.d/99-unprivileged-port.conf
 # Enable linger for rootless user
 loginctl enable-linger nm
 
-# Enable socket + quadlet by default
+systemctl --user daemon-reload
+
+# Enable socket + quadlet at startup
 systemctl --user enable cyan-proxy.socket
 systemctl --user enable cyan.pod
